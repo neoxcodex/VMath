2e336a70fe25bb8eeb8e9d9239ce109b5ad3c099
neoxcodex <er.oscar.robinson@gmail.com>
2026-02-01 23:45:29 +0530

    Major Changes : Visualization Scaffolding

---
+++ include/VMath/Vis/visualizer.hpp
@@ -0,0 +1,13 @@
+#ifndef visualizer_2890938098035
+#define visualizer_2890938098035
+
+
+namespace visualizer{
+
+
+
+
+
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
+#include "VMath/Math/Constants.h"
+#include "VMath/Math/VMath3D.hpp"
+#include "VMath/Math/VMath2D.hpp"
+#include "VMath/Math/NDSFunctions.hpp"
+#include "VMath/Math/VMathN.hpp"
+// #include "VMath/Vis/visualizer.hpp" // Future home of the visualizer
 
-#include "Constants.h"
-#include"VMath3D.hpp"
-#include"VMath2D.hpp"
-#include"NDSFunctions.hpp"
-#include"VMathN.hpp"
-#include<iostream>
-int main(){
-   dim3::Vec3 v1{1.0f, 0.0f, 0.0f};
-   dim3::Vec3 v2{0.0f, 1.0f, 0.0f};
-   dim3::Vec3 newVec = dim3::crossProduct(v1, v2);
-   dim3::vectorCout(newVec);
-   if(NDSF::epEqual(dim3::angleRAD(newVec, v1), (constants::PI / 2.0f))){
-      std::cout << "Yeah its okay!" << std::endl;
-   }
+#include <iostream>
+
+// Create an alias if it's not defined in the header
+
+
+int main() {
+    // Using dim3 namespace from your VMath3D.hpp
+    dim3::Vec3 v1{1.0f, 0.0f, 0.0f};
+    dim3::Vec3 v2{0.0f, 1.0f, 0.0f};
+
+    // Math check: Cross product of X and Y should be Z (0, 0, 1)
+    dim3::Vec3 crossed = dim3::crossProduct(v1, v2);
+
+    // Check angle between Z and X (should be 90 degrees / PI/2)
+    float angle = dim3::angleRAD(crossed, v1);
+
+    if (NDSF::epEqual(angle, constants::PI / 2.0f)) {
+        std::cout << "YAY IT IS 90 DEG" << std::endl;
+        dim3::vectorCout(crossed);
+    } else {
+        std::cout << "Math check failed. Angle: " << angle << std::endl;
+    }
+
+    return 0;
 }
